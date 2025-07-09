7ae5a35aa42d0f56740775b20c473b562701bdf6
Shel Soloa <shel@shelsoloa.com>
2025-07-09 15:59:18 -0400

    nit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -255,7 +255,7 @@ function AboutUs() {
 function Contact() {
   return (
     <Section className="bg-accent text-white" id="contact">
-      <Card>
+      <Card className="max-w-4xl mx-auto">
         <CardHeader>
           <CardTitle>
             <SectionTitle>Contact Us</SectionTitle>
