f5caf942425eeb0bdc736b0f039865ea99207a8e
Shel Soloa <shel@shelsoloa.com>
2025-07-13 22:21:43 -0400

    hide formdata

--- src/app/forms.tsx
+++ src/app/forms.tsx
@@ -9,7 +9,7 @@ export function ContactForm() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
+    // const formData = new FormData(e.target as HTMLFormElement);
     // const { error } = await sendContactEmail(formData);
 
     const error = "Not implemented";
