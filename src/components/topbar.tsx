b395a0913c1731359f973554bdcbd4bd478598d3
Shel Soloa <shel@shelsoloa.com>
2025-07-17 11:27:25 -0400

    Fix topbar on mobile

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 export function Topbar() {
   return (
     <div className="bg-black p-8">
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
+      <div className="max-w-7xl mx-auto flex items-center justify-between sm:flex-row flex-col gap-4">
         <Link
           href="#hero"
           className="hover:opacity-75 transition-all duration-300"
@@ -13,7 +13,7 @@ export function Topbar() {
           <BraneBridgeLogo />
         </Link>
 
-        <div className="flex items-center w-1/2 justify-between text-primary-foreground">
+        <div className="flex items-center sm:w-1/2 w-full justify-between text-primary-foreground">
           <Link href="#services" className="hover:text-primary">
             SERVICES
           </Link>
